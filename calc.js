6f07f8102a87b0a18a0615f05eccfdc5d3b70711
benjaminfester <benfesterh@gmail.com>
2021-12-15 11:09:51 +0100

    transfer to work comp

---
+++ calc.js
@@ -0,0 +1,15 @@
+console.log("yo")
+
+
+// variables
+
+// 
+var gamma_c = 1.5;
+var gamma_s = 1.2;
+
+console.log(gamma_c);
+
+//N observations: 
+
+// lmd_known can be locked off
+// lmd_known == "on" : 
